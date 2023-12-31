2649ac927df36b00b3cb77b94885c70156c176f4
Kyaw Oo <kyawkyawjek@gmail.com>
2023-12-31 21:01:28 +0630

    Admin logo

--- admin/src/app/layout.tsx
+++ admin/src/app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Snack Zone Admin dashboard",
   description: "Developed by Kko",
+  icons: {
+    icon: "/favico.png",
+  },
 };
 
 export default function RootLayout({
